1d2564d42c21ba6e5d4e32f2376535d7a08ce3aa
TheGermanPyro <cwall@kapoleihigh.org>
2017-12-12 09:13:36 -1000

    addition of key 2

--- 1_3_9-10_sourceFiles/Always Gonna Break (So Should You).py
+++ 1_3_9-10_sourceFiles/Always Gonna Break (So Should You).py
@@ -1,38 +1,45 @@
 ####
 # Each team's file must define four tokens:
-#     team_name: a string
+#     team_name: 15
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
 ####
 
-team_name = 'The name the team gives to itself' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+team_name = '' # Only 10 chars displayed.
+strategy_name = 'The KEY to success'
+strategy_description = "The way in which this system works is that the system checks the first 10 responses of a person to determine if the person has the key. If they do, then the system will collude with them the other 89 times. The final move will be a betray to act as a minor addition, so that every successful key will increase my score by 100. If at any point the key doesnt work or the person betrays, a failsafe is initiated, and the code reverts to a predetermined algorithm which determines the other user's algorithm with a sample size of ten moves."
     
 def move(my_history, their_history, my_score, their_score):
+    th = their_history
     ''' Arguments accepted: my_history, their_history are strings.
     my_score, their_score are ints.
     
     Make my move.
     Returns 'c' or 'b'. 
     '''
-    th = their_history
-    theirDictionary = {}
-    previousTrial = 0
-    currentTrial = 0
+    currentTrialLength = len(th)
+    dicStepper = 0
     desicion = ''
-    while previousTrial < len(th):
-        theirDictionary['trial{0}'.format(previousTrial)] = th[previousTrial]
-        previousTrial = previousTrial + 1
-    if len(theirDictionary) > 0:
-        if theirDictionary[trial{0}.format(currentTrial)] == 'c':
-            if theirDictionary[trial{0}.format(currentTrial+1)] == 'c':
-                desicion = 'b'
-            
-            
-    else:
-        desicion = 'b'
+    previousTrialDesicion = th()
+    theirDictionary = {}
+    publicKey = {"key0":"c", "key1":"c", "key2":"b", "key3":"b","key4":"c","key5":"b","key6":"c","key7":"b","key8":"b","key9":"c"}
+    privateKey = {"key0":"b", "key1":"b", "key2":"c", "key3":"c","key4":"b","key5":"c","key6":"b","key7":"c","key8":"c","key9":"b"}
+    keyActivated = False
+    keyReject = False
+    while dicStepper < len(th):
+        theirDictionary['trial{0}'.format(dicStepper)] = th[dicStepper]
+        dicStepper = dicStepper + 1
+    while currentTrial > 10 and keyReject == False:
+        if currentTrial == 0:
+            desicion = privateKey['key{0}'.format(currentTrial)]
+        elif currentTrial < 0:
+            if theirDictionary['trial{0}'.format(previousTrial)] == publicKey['key{0}'.format(previousTrial)]:
+                desicion = privateKey['key{0}'.format(currentTrial)]
+            else:
+                desicion = fail_safe()
+                
+                
         
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
@@ -44,6 +51,8 @@ def move(my_history, their_history, my_score, their_score):
     
     return desicion
 
+def fail_safe():
+    return "b"
     
 def test_move(my_history, their_history, my_score, their_score, result):
     '''calls move(my_history, their_history, my_score, their_score)
